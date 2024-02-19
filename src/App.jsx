601c0451a1be014cdf13990784956df2dce17f9f
Lesedi <lsd1code@gmail.com>
2024-02-19 18:54:36 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="min-h-screen bg-very-light-gray text-very-dark-blue">
+      Hello world
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'bright-blue': 'hsl(220, 98%, 61%)',
+        'very-light-gray': 'hsl(0, 0%, 98%)',
+        'very-light-grayish-blue': 'hsl(236, 33%, 92%)',
+        'light-grayish-blue': 'hsl(233, 11%, 84%)',
+        'dark-grayish-blue': 'hsl(236, 9%, 61%)',
+        'very-dark-grayish-blue': 'hsl(235, 19%, 35%)',
+        'very-dark-blue': 'hsl(235, 21%, 11%)',
+        'very-dark-saturated-blue': 'hsl(235, 24%, 19%)',
+        'light-grayish-blue-darkmode': 'hsl(234, 39%, 85%)',
+        'light-grayish-blue-darkmode-hover': 'hsl(236, 33%, 92%)',
+        'dark-grayish-blue-darkmode': 'hsl(234, 11%, 52%)',
+        'very-dark-grayish-blue-darkmode': 'hsl(233, 14%, 35%)',
+        'very-dark-grayish-blue-darkmode-': 'hsl(237, 14%, 26%)',
+      }
+    },
+  },
+  plugins: [],
+}
